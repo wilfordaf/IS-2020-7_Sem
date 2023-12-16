be50d17a9accd72fdde7775a9f3943590e43471c
Sergey Yurpalov <79761555+wilfordaf@users.noreply.github.com>
2023-12-16 21:50:57 +0300

    feat: devops 4-5 labs

---
+++ Devops/Lab 4/client/config_gen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# First argument: Client identifier
+KEY_DIR=/etc/openvpn/client
+OUTPUT_DIR=/root
+BASE_CONFIG=/root/openvpn-ca/client.conf
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${KEY_DIR}/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${KEY_DIR}/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${KEY_DIR}/${1}.key \
+    <(echo -e '</key>\n<tls-crypt>') \
+    ${KEY_DIR}/ta.key \
+    <(echo -e '</tls-crypt>') \
+    > ${OUTPUT_DIR}/${1}.ovpn
